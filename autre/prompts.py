2e1df8ce3795d1bde311809c33fb70b32f2a52a4
amarg <giulia.amar@edu.esiee.fr~>
2026-05-16 12:13:30 +0200

    add few shots

---
+++ autre/prompts.py
@@ -0,0 +1,9 @@
+FEW_SHOT_USER = """### CONTEXT:
+ID: 12 | Paragraph: L'entreprise X a été fondée en 2010 par Jean Dupont.
+ID: 15 | Paragraph: Jean Dupont a nommé Marie Courtois directrice générale en 2018.
+
+### QUESTION:
+Qui est la directrice générale de l'entreprise X ?"""
+
+FEW_SHOT_ASSISTANT = """<chunks_id>12, 15</chunks_id>
+<answer>Marie Courtois est la directrice générale de l'entreprise X.</answer>"""
